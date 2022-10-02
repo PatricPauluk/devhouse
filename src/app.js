4abef840c08d052e63df77561c2632fe51cf601c
Patric Pauluk <patric.pauluk@outlook.com>
2022-10-02 00:08:05 -0300

    conectado ao mongodb

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes'; // requisita o arquivo routes.js (não é uma dependência!)
 
-/* Notas da configuração da execução do servidor
+
+/* ---------------------------- Notas Gerais de Configuração ----------------------------
+
 
 -----------------------------------------------------------------------------------------
 | yarn add sucrase nodemon -D                                                           |
@@ -11,24 +14,51 @@ import routes from './routes'; // requisita o arquivo routes.js (não é uma dep
 | -D      | instala como dependência de desenvolvedor                                   |
 -----------------------------------------------------------------------------------------
 
-O server não executa como antes (node src/server.js) após as atualizações com Sucrase,
-por causa do import. Executa-se com o Sucrase: yarn sucrase-node src/server.js
 
-Para executar com o yarn dev (junto com nodemon):
-1. Script no package.json;
-2. Criar um arquivo nodemon.json na raiz;
-3. No arquivo nodemon.json será buscado e registrado os arquivos .js necessários.
-4. O projeto pode ser executado com yarn dev;
+-----------------------------------------------------------------------------------------
+| O server não executa como antes (node src/server.js) após as atualizações com Sucrase |
+| por causa do import. Executa-se com o Sucrase: yarn sucrase-node src/server.js        |
+-----------------------------------------------------------------------------------------
+| Para executar com o yarn dev (junto com nodemon):                                     |
+| 1. Script no package.json;                                                            |
+| 2. Criar um arquivo nodemon.json na raiz;                                             |
+| 3. No arquivo nodemon.json será buscado e registrado os arquivos .js necessários;     |
+| 4. O projeto pode ser executado com yarn dev.                                         |
+-----------------------------------------------------------------------------------------
 
 
-Qualquer dúvida, comparar o código com o "crud" criado em my_study, código mais antigo.
+Utilizando MongoDB: https://www.mongodb.com/atlas/database
 
-*/
+1.  No site foi permitido todos os IP's na opção Network Access, criado um usuário na
+    opção Database Access, e o cluster/banco devhouse foi criado em Database.
+
+2.  No banco, clica na opção Connect, e depois em Connect your application, salvamos
+    esta connection string:
+    mongodb+srv://papauluk:<password>@devhouse.hyory8h.mongodb.net/?retryWrites=true&w=majority
+
+3.  Instalada a Biblioteca Mongoose:
+    yarn add mongoose
+
+4. Após importar o Mongoose, a conexão ao banco é realizada dentro do método constructor.
+
+
+------------------------------------ Fim das Notas ------------------------------------ */
+
 
 class App{
     // quando a classe app for chamada, o método construtor é o primeiro a ser iniciado automáticamente
     constructor(){
-        this.server = express(); // o this é necessário para referenciar a classe App
+        // o this é necessário para referenciar a classe App
+        this.server = express(); 
+        
+        // connection string
+        const URI = 'mongodb+srv://papauluk:SYTwbyWg59L05zmx@devhouse.hyory8h.mongodb.net/?retryWrites=true&w=majority';
+        
+        // conecta ao banco de dados mongodb
+        mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
         // middlewares e routes devem ser chamados no constructor para serem iniciados
         this.middlewares();
@@ -36,11 +66,13 @@ class App{
     }
 
     middlewares(){
-        this.server.use(express.json()); // consta que vai usar JSON no express
+        // consta que vai usar JSON no express
+        this.server.use(express.json());
     }
 
     routes(){
-        this.server.use(routes); // chama as rotas da requisição da 2ª linha
+        // chama as rotas da importação
+        this.server.use(routes); 
     }
 
 }
